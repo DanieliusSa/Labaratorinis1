8f437d467d5b665d6a67bc84aa683c0038bf84ee
DanieliusSa <danielsavickas@gmail.com>
2022-06-10 17:49:41 +0300

    Labaratorinis 1.0

--- app/src/main/java/com/example/labaratorinis1/MainActivity.java
+++ app/src/main/java/com/example/labaratorinis1/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.labaratorinis1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnMainClick(View view) {
+        TextView tvMain = (TextView)findViewById(R.id.tvMain);
+        tvMain.setText("smth new");
+    }
+
+    public void onBtnColorClick(View view) {
+        TextView tvMain = (TextView) findViewById(R.id.tvMain);
+        tvMain.setTextColor(Color.parseColor("#FF0000"));
     }
 }
